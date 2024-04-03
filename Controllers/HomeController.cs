1559b2093d40f1ffae44e5ffd4ce3a9643f6c75b
Dhananjay Gautam <37128287+Dhananjay1437@users.noreply.github.com>
2024-04-03 17:37:56 +0530

    added first controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using ModelValidationsExample.Models;
+
+namespace DotNetCoreBasicProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        
+        //Example JSON: { "PersonName": "William", "Email": "william@example.com", "Phone": "123456", "Password": "william123", "ConfirmPassword": "william123","Age":22 }
+       
+        //[ModelBinder(BinderType = typeof(PersonModelBinder))]   //add this in function if you want to use custom modal binder
+      
+        // public IActionResult Index([Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))]  Person person)   //only selected field will get from user
+
+        // public IActionResult Index(Person person)   //use for form-data,x-www-urlincoding or query binding if you want
+       
+        public IActionResult Index([FromBody] Person person, [FromHeader(Name = "User-Agent")] string UserAgent)
+        {
+            if (!ModelState.IsValid)
+            {
+                //get error messages from model state
+                string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
+
+                return BadRequest(errors);
+            }
+
+            return Content($"{person},{UserAgent}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+using DotNetCoreBasicProject.CustomMiddleware;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<MyCustomMiddleware>();
+builder.Services.AddControllers().AddXmlSerializerFormatters();
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+//simple  play with asp .net
+
+/*app.Run(async (HttpContext context) =>
+{
+    String path = context.Request.Path;
+    String method = context.Request.Method;
+    await context.Response.WriteAsync("From Middleware 3");
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p></br>");
+        }
+    }
+    if (context.Request.Headers.ContainsKey("User-Agent"))
+    {
+        string userAgent = context.Request.Headers["User-Agent"];
+        await context.Response.WriteAsync($"<p>{userAgent}</p></br>");
+    }
+    await context.Response.WriteAsync($"<p>{path}</p></br>");
+    await context.Response.WriteAsync($"<p>{method}</p></br>");
+});*/
+
+// middleware with asp .net
+
+//middlware 1
+/*app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("From Midleware 1");
+    await next(context);
+});
+
+
+//middleware 2
+//app.UseMiddleware<MyCustomMiddleware>();
+app.UseMyCustomMiddleware();
+app.UseHelloCustomMiddleware();
+//middleware 3
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("From Middleware 3");
+});*/
+
+//Routing in .net
+//enable routing
+
+/*app.UseRouting();
+app.Use(async (context, next) =>
+{
+    Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
+    if (endPoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint: {endPoint.DisplayName}\n");
+    }
+
+    await next(context);
+});
+
+//creating endpoints
+app.UseEndpoints(endpoints =>
+{
+    //add your endpoints here
+    endpoints.MapGet("map1", async (context) => {
+        await context.Response.WriteAsync("In Map 1");
+    });
+
+    endpoints.MapPost("map2", async (context) => {
+        await context.Response.WriteAsync("In Map 2");
+    });
+});
+
+app.Run(async context => {
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+});*/
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
